181b4274218876a780c4952d72518a8e12816960
logonrm <logonrm@fiap.com.br>
2017-09-14 19:21:30 -0300

    update

---
+++ 04-SpringMVC-Exercicio/src/br/com/fiap/model/Carro.java
@@ -0,0 +1,45 @@
+package br.com.fiap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name="seqCarro",sequenceName="SQ_CARRO",allocationSize=1)
+public class Carro {
+
+	@Id
+	@GeneratedValue(generator="seqCarro",strategy=GenerationType.SEQUENCE)
+	private int codigo;
+	
+	private String modelo;
+	
+	private int ano;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+}
